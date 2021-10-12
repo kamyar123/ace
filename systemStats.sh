a25a39adc6ac1702c066f729a4edafac1168080f
kamyar123 <kh5052n@gre.ac.uk>
2021-10-12 17:46:36 +0100

    created cpu usage

--- systemStats.sh
+++ systemStats.sh
@@ -13,7 +13,7 @@ RELEASED="5/10/2021"
 
 USAGE(){ 
 	echo -e $1
-	echo -e "\nUssage: systemStats [-t temperature] [-i ipv4 address]"
+	echo -e "\nUssage: systemStats [-t temperature] [-i ipv4 address] [-c cpu usage]"
 	echo -e "\t\t [-v version]"
  	echo -e "\t\t more i8nformation see man systemStats"
 }
@@ -25,7 +25,7 @@ if [ $# -lt 1 ];then
 elif [ $# -gt 3 ];then
  	USAGE" too many arguments supplies"
 	exit 1
-elif [[ ( $1 == '-h' ) || ( $1 =='--help' ) ]];then
+elif [[ ( $1 == '-h' ) || ( $1 == '--help' ) ]];then
 	USAGE "help!"
 	exit 1
 fi
@@ -33,12 +33,17 @@ fi
 # frequently a scripts are written so that arguments can be passed in any order using 'flag'
 # with the flags method , some of the arguments can be  made mandatory or optional
 # a:b (a is mandator, b is  optional ) abc is all optional 
-while getopts tiv OPTION
+while getopts ctiv OPTION
 do 
 case ${OPTION}
 in
 i)  IP=$(ifconfig wlan0 | grep -w inet | awk '{print$2'}
 	echo ${IP}
+c) USAGE=$(grep -w 'cpu' /proc/stat | awk '{usage=($2+$3+$4+$6+$7+$8)*100/($2+$3+$4+$5+$6+$7+$8)}
+						{free=($5)*100/($2+$3+$4+$5+$6+$7+$8)} 
+				                END{printf"Used cpu: %.2f%%\n",usage} 
+					           {printf"Free cpu: %.2f%%\n",free}')
+	echo ${USAGE};;
 t) TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
 	echo ${TEMP} "need to divide by thousend...";;
 v)	echo -e "systemStats:\n\t version: ${VERSION} Released: ${RELEASED} Author: ${AUTHOR}";;
